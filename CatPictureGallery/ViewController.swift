1f6088ec916c2c6b5fbfd98e8ece6f9b0a8df7fc
Gustavo Vilela <gfvilela@gmail.com>
2023-03-08 20:04:13 -0300

    initial commit

---
+++ CatPictureGallery/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CatPictureGallery
+//
+//  Created by Gustavo Freitas Vilela on 08/03/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Pods/Target Support Files/Pods-CatPictureGallery-CatPictureGalleryUITests/Pods-CatPictureGallery-CatPictureGalleryUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_CatPictureGallery_CatPictureGalleryUITests : NSObject
+@end
+@implementation PodsDummy_Pods_CatPictureGallery_CatPictureGalleryUITests
+@end
