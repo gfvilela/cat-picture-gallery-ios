6f4ba78aff31b807c50b6be094e2fb0ae5738413
Gustavo Vilela <gfvilela@gmail.com>
2023-03-10 16:01:31 -0300

    add activity indicator

--- CatPictureGallery/Presentation/ImagesList/View/ImagesListViewController.swift
+++ CatPictureGallery/Presentation/ImagesList/View/ImagesListViewController.swift
@@ -8,14 +8,17 @@
 import UIKit
 
 class ImagesListViewController: UIViewController {
+    @IBOutlet weak var loadingView: UIStackView!
     @IBOutlet weak var imagesListCollectionView: ImagesListCollectionView!
     lazy var viewModel: ImagesListViewModel = ImagesListViewModelImpl(actions: self)
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        self.loadingView.isHidden = false
         viewModel.items = { [weak self] images in
             DispatchQueue.main.async {
+                self?.loadingView.isHidden = true
                 self?.imagesListCollectionView.items = images
             }
         }
