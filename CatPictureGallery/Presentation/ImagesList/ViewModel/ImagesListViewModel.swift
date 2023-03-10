48defb2aa9ec364ac00ddafac0b2f3391315c211
Gustavo Vilela <gfvilela@gmail.com>
2023-03-09 22:08:05 -0300

    add image list view

---
+++ CatPictureGallery/Presentation/ImagesList/ViewModel/ImagesListViewModel.swift
@@ -0,0 +1,16 @@
+//
+//  ImagesListViewModel.swift
+//  CatPictureGallery
+//
+//  Created by Gustavo Freitas Vilela on 09/03/23.
+//
+
+import Foundation
+
+protocol ImagesListViewModelInput {
+    func didSelect(index: Int)
+}
+
+protocol ImagesListViewModelOutput {
+//    var items: 
+}
